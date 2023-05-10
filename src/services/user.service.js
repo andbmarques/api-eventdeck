43e0dd8c353a05071258a4fbc373c46c451de664
Anderson B <andbmarques@gmail.com>
2023-05-10 18:11:36 -0300

    :star: Add | Update User Route

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,9 +3,15 @@ const User = require("../models/User");
 const createService = (body) => User.create(body);
 const findAll = () => User.find();
 const findById = (id) => User.findById(id);
+const update = (id, name, username, email, role, docId, password) =>
+  User.findOneAndUpdate(
+    { _id: id },
+    { name, username, email, role, docId, password }
+  );
 
 module.exports = {
   createService,
   findAll,
   findById,
+  update,
 };
