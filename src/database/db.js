f4b724822399065bc37d5449bcd75cde9a585ca9
Anderson B <andbmarques@gmail.com>
2023-05-10 16:57:32 -0300

    :star: Add | Implementation of the first user routes

--- src/database/db.js
+++ src/database/db.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const connectDb = () => {
   mongoose
     .connect(
-      `mongodb+srv://@cluster0.dywuuru.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.dbName}:${process.env.dbPassword}@cluster0.dywuuru.mongodb.net/?retryWrites=true&w=majority`
     )
     .then(() => {
       console.log("\nDB Connected");
     })
     .catch((err) => {
-      console.log("\n DB Error" + err);
+      console.log("\n DB Error:" + err);
     });
 };
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,4 +3,8 @@ const userController = require("../controllers/user.controller");
 
 route.post("/", userController.create);
 
+route.get("/", userController.findAllUsers);
+
+route.get("/:id", userController.findById);
+
 module.exports = route;
