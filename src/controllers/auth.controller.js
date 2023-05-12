7da6ef034efde7440aa917406706ba56dc04e3a1
Anderson B <andbmarques@gmail.com>
2023-05-12 11:51:11 -0300

    :star: Add | JWT on SignIn and New Model Events

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import bcrypt from "bcrypt";
-import { login as loginService } from "../services/auth.service.js";
+import {
+  generateToken,
+  login as loginService,
+} from "../services/auth.service.js";
 
 const login = async (req, res) => {
   try {
@@ -20,6 +23,10 @@ const login = async (req, res) => {
         .status(404)
         .json({ msg: "Not found | Email or password not found" });
     }
+
+    const token = generateToken(user.id);
+
+    res.status(200).json({ token });
   } catch (error) {
     res.status(500).send(error.message);
   }
