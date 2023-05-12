8dfbf0ca81db68227b4b663f478720529b225e82
Anderson B <andbmarques@gmail.com>
2023-05-12 16:27:27 -0300

    :star: Add | Pagination on Events List

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -1,4 +1,8 @@
-import { createService, findAllService } from "../services/events.service.js";
+import {
+  createService,
+  findAllService,
+  countEvents,
+} from "../services/events.service.js";
 import checkLength from "../utils/checkLength.js";
 
 const create = async (req, res) => {
@@ -27,7 +31,30 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
   try {
-    const events = await findAllService();
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+      limit = 5;
+    }
+    if (!offset) {
+      offset = 0;
+    }
+
+    const events = await findAllService(offset, limit);
+    const total = await countEvents();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
 
     if (checkLength(events) === 0) {
       return res
@@ -35,7 +62,9 @@ const findAll = async (req, res) => {
         .json({ msg: "Not found: There are no registered events yet." });
     }
 
-    res.status(200).json(events);
+    res
+      .status(200)
+      .json({ results: events, nextUrl, previousUrl, limit, offset, total });
   } catch (error) {
     console.log("\n\x1b[31m[Server Error] " + error.message + "\x1b[0m\n");
     return res.status(500).json({ msg: "Server Error: " + error.message });
--- src/services/events.service.js
+++ src/services/events.service.js
@@ -1,6 +1,12 @@
 import Events from "../models/Events.js";
 
 const createService = (body) => Events.create(body);
-const findAllService = () => Events.find();
+const findAllService = async (offset, limit) =>
+  await Events.find()
+    .sort({ _id: -1 })
+    .skip(offset)
+    .limit(limit)
+    .populate("producer");
+const countEvents = () => Events.countDocuments();
 
-export { createService, findAllService };
+export { createService, findAllService, countEvents };
