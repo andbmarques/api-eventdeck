f2e18a9e8f7a987a7e5f79bb79a8c84aa5e654b2
Anderson B <andbmarques@gmail.com>
2023-05-16 16:26:39 -0300

    :gear: Fix | CORS

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const connectDb = require("./database/db"); */
 import express from "express";
 import connectDb from "./database/db.js";
 import { config } from "dotenv";
+import cors from "cors";
 
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
@@ -20,6 +21,7 @@ const PORT = process.env.PORT || 4000;
 connectDb();
 
 app.use(express.json());
+app.use(cors());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/events", eventsRoute);
